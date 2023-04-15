1a635c8a11aa7d3a727e7bc132ab281652123397
Diego Revilla <diego.revilla.r@gmail.com>
2023-04-16 01:51:49 +0200

    Globals

--- CS300/Global.h
+++ CS300/Global.h
@@ -1,13 +1,19 @@
+//
+//	Global.h
+//	OpenGL Graphics
+//
+//	Created by Diego Revilla on 15/04/23
+//	Copyright © 2023. All Rights reserved
+//
+
 #ifndef _GLOBAL__H_
 #define _GLOBAL__H_
 
 template <typename T>
 class Singleton {
 public:
-    static T& Instance() {
-        static T instance;
-        return instance;
-    }
+    static T& Instance();
+    
 protected:
     Singleton() {}
     ~Singleton() {}
@@ -16,5 +22,17 @@ public:
     Singleton(Singleton const&) = delete;
     Singleton& operator=(Singleton const&) = delete;
 };
+
+// ------------------------------------------------------------------------
+/*! Instance
+*
+*   Returns THE instance of the global object
+*/ //----------------------------------------------------------------------
+template<typename T>
+T& Singleton<T>::Instance() {
+    static T instance;
+
+    return instance;
+}
 
 #endif
